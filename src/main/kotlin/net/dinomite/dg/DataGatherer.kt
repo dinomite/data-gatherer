551a8267fa076160efc2eaf57b6c54c40e4b796b
Drew Stephens <drew@dinomite.net>
2020-01-04 14:16:34 -0500

    Use ServiceManager

--- src/main/kotlin/net/dinomite/dg/DataGatherer.kt
+++ src/main/kotlin/net/dinomite/dg/DataGatherer.kt
@@ -3,6 +3,7 @@
 package net.dinomite.dg
 
 import com.google.common.util.concurrent.Service
+import com.google.common.util.concurrent.ServiceManager
 import kotlinx.coroutines.runBlocking
 import net.dinomite.dg.services.TimeService
 import org.apache.commons.configuration2.CompositeConfiguration
@@ -10,49 +11,39 @@ import org.apache.commons.configuration2.EnvironmentConfiguration
 import org.apache.commons.configuration2.PropertiesConfiguration
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
 import org.apache.commons.configuration2.builder.fluent.Parameters
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Files
 import java.nio.file.Path
-
-class DataGatherer(config: Settings) {
-    companion object {
-        private val logger = LoggerFactory.getLogger(this::class.java.name)
-    }
-
-    private val services = listOf<Service>(
-            TimeService(config)
-    )
-
-    fun start() = services.forEach {
-        logger.info("Starting ${it.javaClass.simpleName}")
-        it.startAsync()
-        logger.debug("Started ${it.javaClass.simpleName}")
-    }
-
-    fun stop() = services.forEach {
-        logger.info("Stopping ${it.javaClass.simpleName}")
-        it.stopAsync()
-        logger.debug("Stopped ${it.javaClass.simpleName}")
-    }
-
-    fun isRunning(): Boolean = services.firstOrNull { it.isRunning } != null
-}
+import java.time.Duration
+import java.util.concurrent.TimeoutException
 
 const val USAGE = """
 USAGE:
     DataGathererKt CONFIG_DIR
 """
+val STOP_DURATION: Duration = Duration.ofSeconds(5)
+
+val logger: Logger = LoggerFactory.getLogger("DataGatherer app")
 
 fun main(args: Array<String>) {
     val config = buildConfiguration(args)
 
-    val app = DataGatherer(config)
-    Runtime.getRuntime().addShutdownHook(shutdownHook(app))
+    val services = listOf<Service>(
+            TimeService(config)
+    )
 
-    app.start()
-    while (app.isRunning()) {
-        Thread.sleep(1000)
-    }
+    val serviceManager = ServiceManager(services)
+    serviceManager.addListener(object : ServiceManager.Listener() {
+        override fun failure(service: Service) {
+            logger.warn("Failed to stop ${service.javaClass.simpleName}")
+            super.failure(service)
+        }
+    })
+    Runtime.getRuntime().addShutdownHook(shutdownHook(serviceManager))
+
+    serviceManager.startAsync()
+    serviceManager.awaitStopped()
 }
 
 private fun buildConfiguration(args: Array<String>): Settings {
@@ -74,10 +65,14 @@ private fun buildConfiguration(args: Array<String>): Settings {
     }.let { Settings(it) }
 }
 
-private fun shutdownHook(dataGatherer: DataGatherer) = object : Thread() {
+private fun shutdownHook(serviceManager: ServiceManager) = object : Thread() {
     override fun run() = runBlocking {
-        println("Shutting down")
-        dataGatherer.stop()
-        println("Done")
+        logger.info("Shutting down")
+        try {
+            serviceManager.stopAsync().awaitStopped(STOP_DURATION)
+        } catch (e: TimeoutException) {
+            logger.warn("Timed out while stopping services", e)
+        }
+        logger.info("Done")
     }
 }
