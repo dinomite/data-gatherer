7cc8ad1a8c54429789fd36ed09d977535da47358
Drew Stephens <drew@dinomite.net>
2020-02-17 10:31:14 -0500

    Add timing to base service class

--- src/main/kotlin/net/dinomite/dg/services/EmonScheduleService.kt
+++ src/main/kotlin/net/dinomite/dg/services/EmonScheduleService.kt
@@ -4,15 +4,24 @@ import com.google.common.util.concurrent.AbstractScheduledService
 import kotlinx.coroutines.runBlocking
 import net.dinomite.dg.DataGathererConfig
 import net.dinomite.dg.emon.EmonClient
+import org.slf4j.LoggerFactory
 import java.time.Duration.ZERO
+import kotlin.system.measureTimeMillis
 
 @Suppress("UnstableApiUsage")
 class EmonScheduleService(private val config: DataGathererConfig,
                           private val producer: EmonUpdateProducer,
                           private val emonClient: EmonClient) : AbstractScheduledService() {
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java.name)
+    }
+
     override fun scheduler(): Scheduler = Scheduler.newFixedRateSchedule(ZERO, config.SCHEDULED_SERVICE_INTERVAL)
 
     override fun runOneIteration() = runBlocking {
-        emonClient.sendUpdate(producer.buildUpdate())
+        val time = measureTimeMillis {
+            emonClient.sendUpdate(producer.buildUpdate())
+        }
+        logger.debug("Iteration took $time ms")
     }
 }
