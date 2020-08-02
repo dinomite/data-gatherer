8db7dcf8e78c3c69f06ca0f9ae794fc1caefa90d
Drew Stephens <drew@dinomite.net>
2020-08-02 14:55:53 -0400

    Upgrade dependency & gradle versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     application
+    id("com.github.ben-manes.versions") version "0.29.0"
 }
 
 repositories {
@@ -15,30 +16,30 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    implementation("org.apache.commons:commons-configuration2:2.6")
+    implementation("org.apache.commons:commons-configuration2:2.7")
     implementation("commons-beanutils:commons-beanutils:1.9.4")
 
-    implementation("com.github.kittinunf.fuel:fuel:2.2.1")
-    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.1")
-    implementation("com.github.kittinunf.fuel:fuel-jackson:2.2.1")
+    implementation("com.github.kittinunf.fuel:fuel:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel-jackson:2.2.3")
 
-    implementation("com.google.guava:guava:28.1-jre")
+    implementation("com.google.guava:guava:29.0-jre")
 
     implementation("org.mpierce.guice.warmup:guice-warmup:0.1")
     implementation("com.google.inject", "guice", "4.2.3")
 
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.10.1")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.10.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.10.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.11.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testImplementation(kotlin("test-junit5"))
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 application {
