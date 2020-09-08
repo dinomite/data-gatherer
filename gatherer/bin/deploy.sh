4c75a33a10998633dd5b01d15d4a51154e1b0b48
Drew Stephens <drew@dinomite.net>
2020-09-08 05:53:58 -0400

    Create tag on deploy

--- gatherer/bin/deploy.sh
+++ gatherer/bin/deploy.sh
@@ -7,3 +7,4 @@ docker build -f Dockerfile.gatherer --tag data-gatherer:"$VERSION" .
 docker stop data-gatherer
 docker rm data-gatherer
 docker run --name data-gatherer --log-opt max-size=15m --restart=always -d data-gatherer:"$VERSION"
+git tag -am "$VERSION" "$VERSION"
