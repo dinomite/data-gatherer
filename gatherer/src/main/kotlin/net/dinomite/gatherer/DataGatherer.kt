31831fa778659fb52e7603d7f74ad521dc97f732
Drew Stephens <drew@dinomite.net>
2021-01-17 06:15:02 -0500

    Encapsulate bits of startup and add some comments

--- gatherer/src/main/kotlin/net/dinomite/gatherer/DataGatherer.kt
+++ gatherer/src/main/kotlin/net/dinomite/gatherer/DataGatherer.kt
@@ -41,32 +41,29 @@ USAGE:
 fun main(args: Array<String>) {
     val start = Instant.now()
 
-    val jacksonStartup = onDedicatedThread {
-        jacksonObjectMapper()
-                .registerModule(JavaTimeModule())
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    }
-    val configStartup = onDedicatedThread {
-        buildConfiguration(args, USAGE) { DataGathererConfig(it) }
-    }
-    val startupFutures = listOf(
-            onDedicatedThread { GuiceWarmup.warmUp() }
-    )
-
-    startupFutures.forEach { it.get() }
+    // Start things up in parallel
+    val jacksonStartup = onDedicatedThread { createObjectMapper() }
+    val configStartup = onDedicatedThread { buildConfiguration(args, USAGE) { DataGathererConfig(it) } }
+    listOf(
+        onDedicatedThread { GuiceWarmup.warmUp() }
+    ).forEach { it.get() }
 
     val injector = setupGuice(jacksonStartup.get(), configStartup.get())
 
-    injector.getInstance(EventBus::class.java).register(injector.getInstance(EmonReporter::class.java))
-    injector.getInstance(EventBus::class.java).register(injector.getInstance(InfluxDbReporter::class.java))
+    // Register the subscribers with the eventBus
+    val eventBus = injector.getInstance(EventBus::class.java)
+    eventBus.register(injector.getInstance(EmonReporter::class.java))
+    eventBus.register(injector.getInstance(InfluxDbReporter::class.java))
 
-    val serviceManager = ServiceManager(listOf(
+    // Create the producers which are periodically runnin Services
+    val serviceManager = ServiceManager(
+        listOf(
             injector.getInstance(HubitatReportingService::class.java),
             injector.getInstance(AwairToEmonReportingService::class.java),
             injector.getInstance(DataProducerReportingService::class.java)
-    ))
-    Runtime.getRuntime().addShutdownHook(shutdownHook(serviceManager))
+        )
+    )
+    Runtime.getRuntime().addShutdownHook(serviceManager.shutdownHook())
 
     println("Startup: ${Duration.between(start, Instant.now()).toMillis()}ms")
 
@@ -74,43 +71,52 @@ fun main(args: Array<String>) {
     serviceManager.awaitStopped()
 }
 
+private fun createObjectMapper() = jacksonObjectMapper()
+    .registerModule(JavaTimeModule())
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+
 private fun setupGuice(objectMapper: ObjectMapper, config: DataGathererConfig): Injector {
     return Guice.createInjector(
-            Stage.PRODUCTION,
-            object : AbstractModule() {
-                override fun configure() {
-                    bind(EmonReporter::class.java)
-                    bind(EmonClient::class.java).to(HttpEmonClient::class.java)
+        Stage.PRODUCTION,
+        object : AbstractModule() {
+            override fun configure() {
+                bind(EmonReporter::class.java)
+                bind(EmonClient::class.java).to(HttpEmonClient::class.java)
 
-                    bind(InfluxDbReporter::class.java)
+                bind(InfluxDbReporter::class.java)
 
-                    bind(HubitatReportingService::class.java)
-                    bind(AwairToEmonReportingService::class.java)
-                    bind(DataProducerReportingService::class.java)
+                bind(HubitatReportingService::class.java)
+                bind(AwairToEmonReportingService::class.java)
+                bind(DataProducerReportingService::class.java)
 
-                    bind(ObjectMapper::class.java).toInstance(objectMapper)
-                    bind(DataGathererConfig::class.java).toInstance(config)
+                bind(ObjectMapper::class.java).toInstance(objectMapper)
+                bind(DataGathererConfig::class.java).toInstance(config)
 
-                    bind(EventBus::class.java).toInstance(AsyncEventBus(Executors.newCachedThreadPool(
+                bind(EventBus::class.java).toInstance(
+                    AsyncEventBus(
+                        Executors.newCachedThreadPool(
                             ThreadFactoryBuilder().setNameFormat("event-bus-%d").build()
-                    )))
+                        )
+                    )
+                )
 
-                    binder().requireAtInjectOnConstructors()
-                    binder().requireExactBindingAnnotations()
-                    binder().requireExplicitBindings()
-                    binder().disableCircularProxies()
-                }
+                binder().requireAtInjectOnConstructors()
+                binder().requireExactBindingAnnotations()
+                binder().requireExplicitBindings()
+                binder().disableCircularProxies()
             }
+        }
     )
 }
 
 private fun <T> onDedicatedThread(builder: () -> T): Future<T> = FutureTask { builder() }.also { Thread(it).start() }
 
-private fun shutdownHook(serviceManager: ServiceManager) = object : Thread() {
+private fun ServiceManager.shutdownHook() = object : Thread() {
     override fun run() = runBlocking {
         logger.info("Shutting down")
         try {
-            serviceManager.stopAsync().awaitStopped(Duration.ofSeconds(5))
+            stopAsync().awaitStopped(Duration.ofSeconds(5))
         } catch (e: TimeoutException) {
             logger.warn("Timed out while stopping services", e)
         }
