37d88e3f76bcfb980e52f0a02c3986e64b82cc2e
Drew Stephens <drew@dinomite.net>
2021-01-17 05:58:34 -0500

    Create EqualsCircularFifoQueue and push equality into there

---
+++ model/src/main/kotlin/net/dinomite/gatherer/model/EqualsCircularFifoQueue.kt
@@ -0,0 +1,26 @@
+package net.dinomite.gatherer.model
+
+import org.apache.commons.collections4.queue.CircularFifoQueue
+import java.util.*
+
+class EqualsCircularFifoQueue<T>(private val queue: CircularFifoQueue<T>) : Queue<T> by queue {
+    constructor(size: Int) : this(CircularFifoQueue<T>(size))
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EqualsCircularFifoQueue<*>
+
+        if (queue.size != other.queue.size) return false
+        queue.forEachIndexed { index, observation ->
+            if (observation != other.queue[index]) return false
+        }
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return queue.hashCode()
+    }
+}
--- model/src/main/kotlin/net/dinomite/gatherer/model/Sensor.kt
+++ model/src/main/kotlin/net/dinomite/gatherer/model/Sensor.kt
@@ -1,46 +1,19 @@
 package net.dinomite.gatherer.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.apache.commons.collections4.queue.CircularFifoQueue
 import java.time.Duration
 import java.time.Instant
 
 data class Sensor(
         @JsonProperty("group") val group: Group,
         @JsonProperty("name") val name: String,
-        @JsonProperty("observations") val observations: CircularFifoQueue<Observation<*>>
+        @JsonProperty("observations") val observations: EqualsCircularFifoQueue<Observation<*>>
 ) {
     constructor(group: Group, name: String, observation: Observation<*>) :
-            this(group, name, CircularFifoQueue<Observation<*>>(10).apply { offer(observation) })
+            this(group, name, EqualsCircularFifoQueue<Observation<*>>(10).apply { offer(observation) })
 
     fun withinLast(duration: Duration): Boolean {
-        if (observations.isNotEmpty() && observations.first().timestamp.isAfter(Instant.now().minus(duration))) {
-            return true
-        }
-        return false
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Sensor
-
-        if (group != other.group) return false
-        if (name != other.name) return false
-        if (observations.size != other.observations.size) return false
-        observations.forEachIndexed { index, observation ->
-            if (observation != other.observations[index]) return false
-        }
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = group.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + observations.hashCode()
-        return result
+        return observations.isNotEmpty() && observations.first().timestamp.isAfter(Instant.now().minus(duration))
     }
 }
 
